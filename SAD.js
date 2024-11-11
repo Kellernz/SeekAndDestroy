49568eca1cd87b4df167d78afe1d3d60f1b765b0
keller <guilhermemoraeskeller@gmail.com>
2024-11-11 20:02:50 -0300

    UPDATE Added "player stats" variable

--- SAD.js
+++ SAD.js
@@ -4,11 +4,22 @@ const mapDirections = [
     [-1, 0], [1, 0], [0, 1], [0, -1],
     [-1, -1], [1, -1], [-1, 1], [1, 1]
 ] //Map cardinal directions 
+//Enemy stats
 const enemySquads = 3
 let enemies = []
-function generateRandomPosition(){
+//Player stats
+let ammo = 12
+let turns = 0
+let gameWon = false
+let gameLost = false
+
+function generateRandomPosition() {
     return {
         x: Math.floor(Math.random() * mapSize),
         y: Math.floor(Math.random() * mapSize)
     }
+} // generates enemy random positions
+function initEnemies() {
+    enemies = []
+    while 
 }
